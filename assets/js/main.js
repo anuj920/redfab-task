10e546e5d6445fc75f99b48f4bdf7715498e3ac8
Anuj Singh <44033673+anuj920@users.noreply.github.com>
2019-06-07 21:44:53 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+function showcate(){
+    var cate= $('input:radio[name="cate"]:checked').val();
+    $('.cate-option').hide();
+    $('.' + cate).show();
+}
+
+function showdiv(){
+    var ad = $('#messagetype').val();
+    $('.mycard').hide();
+    $('.' + ad).show();
+    console.log(ad)
+}
+function showdiv1(){
+    var ad = $('#messagetype1').val();
+    $('.mycard').hide();
+    $('.' + ad).show();
+    console.log(ad)
+}
+function showdiv2(){
+    var ad = $('#messagetype2').val();
+    $('.mycard').hide();
+    $('.' + ad).show();
+    console.log(ad)
+}
+function showdiv3(){
+    var ad = $('#messagetype3').val();
+    $('.mycard').hide();
+    $('.' + ad).show();
+    console.log(ad)
+}
+function showdiv4(){
+    var ad = $('#messagetype4').val();
+    $('.mycard').hide();
+    $('.' + ad).show();
+    console.log(ad)
+}
+function showdiv5(){
+    var ad = $('#messagetype5').val();
+    $('.mycard').hide();
+    $('.' + ad).show();
+    console.log(ad)
+}
